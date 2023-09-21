bc576e7384c06f478a715ac9912c0ad1904127c9
Greg Young <greg@charonvii.dev>
2023-09-20 20:52:26 -0600

    Initial commit 2

--- Extractor.py
+++ Extractor.py
@@ -1,91 +1,98 @@
-#App Store ScreenShot Extractor
-
-from bs4 import BeautifulSoup
-import json
-import re
-import requests
-from lxml import html
+import streamlit as st
 import os
+import json
+import requests
+import zipfile
+from io import BytesIO
 
-# URL to fetch
-url = 'https://apps.apple.com/us/app/planet-fitness-workouts/id399857015'  # Replace with your URL
+# Function to download screenshots and create a ZIP file
+def download_screenshots(url):
+    try:
+        # Fetch the web page content
+        response = requests.get(url)
+        html_content = response.content
 
-# Fetch the web page content
-response = requests.get(url)
-html_content = response.content
+        # Parse the HTML content using lxml
+        parsed_html = html.fromstring(html_content)
 
-# Parse the HTML content using lxml
-parsed_html = html.fromstring(html_content)
+        # Extract content using class name
+        class_name = "we-screenshot-viewer__screenshots"
+        soup = BeautifulSoup(html_content, 'html.parser')
+        element_class = soup.find(class_=class_name)
+        if element_class:
+            # Convert the element to a string
+            html_code = str(element_class)
 
-# Extract content using class name
-class_name = "we-screenshot-viewer__screenshots"
-soup = BeautifulSoup(html_content, 'html.parser')
-element_class = soup.find(class_=class_name)
-if element_class:
-    # Convert the element to a string
-    html_code = str(element_class)
-    # You can use html_code as needed
+            # Parse the HTML code
+            soup = BeautifulSoup(html_code, 'html.parser')
 
-# Now you have the extracted HTML content in html_code
+            # Find all 'srcset' attributes
+            srcset_attributes = soup.find_all(attrs={"srcset": True})
 
-# Parse the HTML code
-soup = BeautifulSoup(html_code, 'html.parser')
+            # Extract URLs from the 'srcset' attributes using regular expressions
+            image_urls = []
 
-# Find all 'srcset' attributes
-srcset_attributes = soup.find_all(attrs={"srcset": True})
+            for srcset in srcset_attributes:
+                urls = re.findall(r'https://\S+\.png', srcset['srcset'])
+                image_urls.extend(urls)
 
-# Extract URLs from the 'srcset' attributes using regular expressions
-image_urls = []
+            # Create a folder to save the downloaded screenshots
+            if not os.path.exists('Screenshots'):
+                os.makedirs('Screenshots')
 
-for srcset in srcset_attributes:
-    urls = re.findall(r'https://\S+\.png', srcset['srcset'])
-    image_urls.extend(urls)
+            # Initialize a counter for incrementing filenames
+            counter = 1
 
-# Convert the list of URLs to a JSON array
-json_data = json.dumps(image_urls)
+            # Loop through each URL and download the images
+            for img_url in image_urls:
+                # Generate a unique filename for each image based on the counter
+                filename = os.path.join('Screenshots', f"{counter:04d}_{os.path.basename(img_url)}")
 
-# Load the JSON data
-image_urls = json.loads(json_data)
+                # Send an HTTP GET request to download the image
+                img_response = requests.get(img_url)
 
-# Filter the URLs to keep only those ending with "600x0w.png"
-filtered_urls = [url for url in image_urls if url.endswith("600x0w.png")]
+                # Check if the request was successful (status code 200)
+                if img_response.status_code == 200:
+                    # Save the image to the local folder
+                    with open(filename, 'wb') as image_file:
+                        image_file.write(img_response.content)
+                    counter += 1
 
-# Convert the filtered list of URLs to a JSON array
-filtered_json_data = json.dumps(filtered_urls)
+            # Create a ZIP file containing the screenshots and Output.json
+            with zipfile.ZipFile('Screenshots.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+                zipf.write('Output.json')
+                for root, _, files in os.walk('Screenshots'):
+                    for file in files:
+                        zipf.write(os.path.join(root, file))
 
-# Save the filtered JSON data to a local file named "Output.json"
-with open("Output.json", "w") as outfile:
-    json.dump(filtered_urls, outfile)
+            return True
+        else:
+            return False
+    except Exception as e:
+        return str(e)
 
-print("Filtered JSON data saved to 'Output.json'")
+# Streamlit app
+st.title("Screenshot Extractor")
 
-# Load the filtered JSON data from 'Output.json'
-with open('Output.json', 'r') as json_file:
-    filtered_urls = json.load(json_file)
+# User input for URL
+url = st.text_input("Enter the URL:")
 
-# Create a folder to save the downloaded screenshots if it doesn't exist
-if not os.path.exists('Screenshots'):
-    os.makedirs('Screenshots')
-
-# Initialize a counter for incrementing filenames
-counter = 1
-
-# Loop through each URL and download the images
-for url in filtered_urls:
-    # Generate a unique filename for each image based on the counter
-    filename = os.path.join('Screenshots', f"{counter:04d}_{os.path.basename(url)}")
-    
-    # Send an HTTP GET request to download the image
-    response = requests.get(url)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Save the image to the local folder
-        with open(filename, 'wb') as image_file:
-            image_file.write(response.content)
-        print(f"Downloaded: {filename}")
+if st.button("Extract"):
+    if not url:
+        st.warning("Please enter a valid URL.")
     else:
-        print(f"Failed to download: {url}")
-    
-    # Increment the counter for the next filename
-    counter += 1
+        result = download_screenshots(url)
+        if result:
+            st.success("Screenshots downloaded and zipped successfully!")
+
+            # Provide download link for the ZIP file
+            with open('Screenshots.zip', 'rb') as f:
+                zip_data = f.read()
+            st.download_button(
+                label="Download Screenshots ZIP",
+                data=zip_data,
+                file_name="Screenshots.zip",
+                key="download_button",
+            )
+        else:
+            st.error("Failed to extract screenshots. Please check the URL.")
