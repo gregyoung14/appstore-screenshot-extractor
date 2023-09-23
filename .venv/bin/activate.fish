9bfc3d94d3a92b3f457ac715037ab69f30691d4b
Greg Young <greg@charonvii.dev>
2023-09-22 18:17:51 -0600

    - New Example

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/greg/appstore-screenshot-extractor/.venv"
+set -gx VIRTUAL_ENV "/Users/greg/stream-demo/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
